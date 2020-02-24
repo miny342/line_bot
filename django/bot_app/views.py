61dc3efcca09fc61320e43b237700d3c00e4459f
miny5509 <keto1320xeon@gmail.com>
2020-02-24 23:46:59 +0900

    オウム返し

--- django/bot_app/views.py
+++ django/bot_app/views.py
@@ -1,8 +1,46 @@
-from django.shortcuts import render
-from django.views import View
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic.base import View
+from django.utils.decorators import method_decorator
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+import os
+import json
+
+ACCESS_TOKEN = os.environ["MY_CHANNEL_ACCESS_TOKEN"]
+CHANNEL_SECRET = os.environ["MY_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
 
 # Create your views here.
 class CallbackView(View):
-    def post(self, request, *args, **kwargs):
-        return HttpResponse()
+    def post(self, request):
+        signature = request.headers["X-Line-Signature"]
+
+        body = request.body.decode(request.encoding)
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            return HttpResponse(status=400)
+
+        return HttpResponse(status=200)
+    
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextMessage(text=event.message.text)
+    )
--- django/line_bot/settings/production.py
+++ django/line_bot/settings/production.py
@@ -3,4 +3,4 @@ from .secret import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["miny-ki.f5.si"]
+ALLOWED_HOSTS = ["miny-ki.f5.si"]+[f"192.168.2.{i}" for i in range(100,200)]
